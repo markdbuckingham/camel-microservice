a8f65eb6fdb0888de96bcb188d70c9f5e8e0741c
markb <markdbuckingham@googlemail.com>
2023-01-21 21:46:13 +0000

    Initial

---
+++ src/test/java/com/example/camelmicroservice/CamelMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camelmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
